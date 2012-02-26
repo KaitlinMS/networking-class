76f971748fc263955aa26d30804154319d87be05
cameronsapp <cameronsapp@960e1cf3-d141-ae18-45fc-a57eff933557>
2012-02-26 19:34:41 +0000

    Tidying up some more.

--- trunk/assn2/common.py
+++ trunk/assn2/common.py
@@ -13,7 +13,13 @@ def random_string(n):
     return ''.join(random.choice(string.ascii_letters + string.digits)
                    for i in range(n))
 
+def pad(s):
+    # Pads the string with 0s in case it's not divisible by CHUNK_SIZE
+    padding = (CHUNK_SIZE - (len(s) % CHUNK_SIZE)) % CHUNK_SIZE
+    return s + '0'*padding
+
 def chunkify(s):
+    s = pad(s)
     return [s[i:i+CHUNK_SIZE] for i in xrange(0, len(s), CHUNK_SIZE)]
 
 def str_xor(s1, s2):
