c1c00c8b9770feef39113918662cfdc98a8edfd2
cameronsapp@gmail.com <cameronsapp@gmail.com@960e1cf3-d141-ae18-45fc-a57eff933557>
2012-02-28 04:22:56 +0000

    Implementing block_cipher_encrypt/decrypt

--- assn2/common.py
+++ assn2/common.py
@@ -6,19 +6,21 @@ HEADER_SIZE = 4
 
 CHUNK_SIZE = 8
 
-# This needs to be a string of length CHUNK_SIZE
+# These need to be strings of length CHUNK_SIZE
 IV = "ranDoM45"
-
-def random_string(n):
-    return ''.join(random.choice(string.ascii_letters + string.digits)
-                   for i in range(n))
+KEY = "l0oKak3y"
 
 def pad(s):
-    # Pads the string with 0s in case it's not divisible by CHUNK_SIZE
+    """
+    Pads the string with 0s in case it's not divisible by CHUNK_SIZE
+    """
     padding = (CHUNK_SIZE - (len(s) % CHUNK_SIZE)) % CHUNK_SIZE
     return s + '0'*padding
 
 def chunkify(s):
+    """
+    Breaks a string into a list of chunks of length CHUNK_SIZE
+    """
     s = pad(s)
     return [s[i:i+CHUNK_SIZE] for i in xrange(0, len(s), CHUNK_SIZE)]
 
@@ -27,3 +29,4 @@ def str_xor(s1, s2):
     # http://stackoverflow.com/questions/2612720/how-to-do-bitwise-exclusive-
     # or-of-two-strings-in-python
     return ''.join(chr(ord(c1)^ord(c2)) for c1,c2 in zip(s1, s2))
+
